715691b2ded0aeb5bde8c3d796c7d8027f431182
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-11-24 05:36:33 +0000

    Learning Update

---
+++ Sum_Natural.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int N, i;
+int Sum = 0;
+printf("Enter N Number");
+scanf("%d", &N);
+
+for(i = 0; i<= N; i++){
+    Sum = Sum + i;
+}
+
+printf("Sum Of N Number = %d", Sum);
+return 0;
+
+}
+
+
+
+
