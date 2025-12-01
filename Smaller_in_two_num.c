71c0aa670cd0fb817375938f0924cae086394977
Ujjwal Yadav <ujjwalkryadav01@gmail.com>
2025-12-01 05:55:15 +0000

    Program To Compare two Number which is smaller

---
+++ Smaller_in_two_num.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter First Number numbers: ");
+    scanf("%d", &num1);
+
+    printf("Enter Second Number numbers: ");
+    scanf("%d", &num2);
+
+    if(num1 < num2){
+        printf("Smaller Number Is = %d", num1);
+    }
+
+    else if(num1 == num2){
+        printf("Both Numbers Are Equal: %d and %d", num1, num2);
+
+    }
+
+    else{
+        printf("Smaller Number Is = %d", num2);
+    }
+     
+    return 0;
+    
+}   
+
